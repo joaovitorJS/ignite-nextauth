7c263d15a948682a9684e8b9196f375dc6ade562
joaovitorJS <joao.vitordeoliveira@hotmail.com>
2022-01-14 20:51:19 -0400

    Saving user data and token in cookies

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,4 +1,7 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
+import { setCookie } from "nookies";
+
 import { api } from "../services/api";
 
 type SignInCredentials = {
@@ -9,16 +12,24 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
+  user: User;
 };  
 
 type AuthProviderProps = {
   children: ReactNode;
 }
 
+type User = {
+  email: string;
+  permitions: string[];
+  roles: string[];
+};
+
 export const AuthContext =  createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
   
   async function signIn({email, password}: SignInCredentials) {
    
@@ -28,7 +39,26 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password
       });  
 
-      console.log(response.data);
+      const { token, refreshToken, permitions, roles} = response.data;
+
+      const optionsCookie = {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      }
+
+      /* setCookie(ctx, name, value, options)
+        ctx no client é null ou undefined
+      */
+      setCookie(undefined, 'nextauth.token', token, optionsCookie);
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, optionsCookie);
+
+      setUser({
+        email,
+        permitions,
+        roles
+      });
+
+      Router.push('/dashboard');
 
     } catch (error) {
       console.log(error);
@@ -36,7 +66,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
